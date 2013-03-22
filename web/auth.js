cbec03f6e305d94a83000eeebac39c9d9fe6aae7
Ricky Cook <mail@thatpanda.com>
2013-03-22 23:26:23 +1100

    Added an nstore user db

--- web/auth.js
+++ web/auth.js
@@ -1,9 +1,10 @@
 var config = require('./config'),
     resource = require('express-resource'),
+    nstore = require('nstore'),
     passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy
 
-
+    dbUsers = nstore.new(config.db.users);
 
 /*****************
  * Help
@@ -21,7 +22,29 @@ passport.use(new LocalStrategy({
     usernameField: 'email',
   },
   function(email, password, done) {
-    return done(null, { email: email });
+    var user;
+    dbUsers.get(email, function(err, doc, key) {
+      if (err) {
+        if (err.message.match(/^Document does not exist for/)) {
+          if (config.registration) {
+            user = { password: password, devices: [] };
+            dbUsers.save(email, user, function(err) {
+              if (err) return done(err);
+              return done(null, user)
+            });
+          } else {
+            return done(null, false, {
+              message: 'User ' + email +
+                ' does not exist and registration is disabled'
+            });
+          }
+        } else {
+          return done(err);
+        }
+      }
+      if (password != doc.password) return done(null, false, { message: 'Password was incorrect' });;
+      return done(null, doc);
+    });
   }
 ));
 
@@ -38,7 +61,7 @@ function setupRoutes(app) {
    * All
    */
   authOkay = function(req, res) {
-    return res.send(200, req.user)
+    return res.send(200, req.user);
   }
   app.post('/auth/local.json',
     passport.authenticate('local', { session:false }),
--- web/config.js
+++ web/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   port: 80,
   baseURL: 'http://localhost',
+  db: {
+  	users: 'users.db'
+  },
+  registration: true
 }
