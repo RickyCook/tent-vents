81ce43a40d886cf83ae525935b1e48423c19dbfd
Ricky Cook <mail@thatpanda.com>
2013-03-23 00:34:00 +1100

    Added encrypted passwords rather than plain text

--- web/auth.js
+++ web/auth.js
@@ -4,7 +4,10 @@ var config = require('./config'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy
 
-    dbUsers = nstore.new(config.db.users);
+    $ = require('jquery'),
+
+    dbUsers = nstore.new(config.db.users),
+    dbKeys = nstore.new(config.db.keys);
 
 /*****************
 * Setup
@@ -14,12 +17,13 @@ passport.use(new LocalStrategy({
 		usernameField: 'email',
 	},
 	function(email, password, done) {
-		var user;
+		var user
+		    cryptPassword = encryptPassword(password);
 		dbUsers.get(email, function(err, doc, key) {
 			if (err) {
 				if (err.message.match(/^Document does not exist for/)) {
 					if (config.registration) {
-						user = { password: password, devices: [] };
+						user = { cryptPassword: cryptPassword, devices: [] };
 						dbUsers.save(email, user, function(err) {
 							if (err) return done(err);
 							return done(null, user)
@@ -35,7 +39,15 @@ passport.use(new LocalStrategy({
 				}
 			}
 
-			if (password != doc.password)
+			if ($.type(doc.cryptPassword) != 'string' && $.type(doc.password) == 'string') {
+				doc.cryptPassword = encryptPassword(doc.password);
+				delete doc.password;
+				dbUsers.save(email, doc, function (err) {
+					if (err) return console.log(err);
+					console.log('Updated to cryptPassword for email ' + email);
+				});
+			}
+			if (cryptPassword != doc.cryptPassword)
 				return done(null, false, { message: 'Password was incorrect' });;
 			return done(null, doc);
 		});
@@ -55,7 +67,7 @@ function setupRoutes(app) {
 	* All
 	*/
 	authOkay = function(req, res) {
-		return res.send(200, req.user);
+		return res.send(200, { email: req.user.email, key: req.user.currentKey });
 	}
 	app.post('/auth/local.json',
 		passport.authenticate('local', { session:false }),
@@ -69,11 +81,22 @@ function setupRoutes(app) {
 	);
 
 }
-
+function getPasswordCrypt() {
+	return config.passwords.cryptoFunc
+		.apply(null, config.passwords.cryptoArgs);
+}
+function encryptPassword(password) {
+	var i;
+	for (i = 0; i < config.passwords.passes; i++) {
+		password = getPasswordCrypt().update(password).digest('hex');
+	}
+	return password;
+}
 /*****************
 * Exports
 */
 module.exports = {
-	ensureAuthenticated: ensureAuthenticated,
-	setupRoutes: setupRoutes
+	setupRoutes: setupRoutes,
+	getPasswordCrypt: getPasswordCrypt,
+	encryptPassword: encryptPassword
 }
--- web/config.js
+++ web/config.js
@@ -2,7 +2,13 @@ module.exports = {
 	port: 80,
 	baseURL: 'http://localhost',
 	db: {
-		users: 'users.db'
+		users: 'users.db',
+		keys: 'keys.db'
 	},
-	registration: true
+	registration: true,
+	passwords: {
+		cryptoFunc: require('crypto').createHmac,
+		cryptoArgs: ['sha1', 'changeme'],
+		passes: 1000
+	}
 }
